d4f354f63e34f276f838116c39d8e2a4c499785b
animeduniya <duniyabarbaad24@gmail.com>
2025-03-03 15:21:03 +0530

    Create AppNavigator.js

---
+++ frontend/navigation/AppNavigator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import CartScreen from "../screens/CartScreen";
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Cart" component={CartScreen} />
+    </Stack.Navigator>
+  );
+}

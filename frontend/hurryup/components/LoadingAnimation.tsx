ff22b4d1ba8cc127cac254b1563723a508f6d05a
animeduniya <duniyabarbaad24@gmail.com>
2025-03-03 15:46:23 +0530

    Create LoadingAnimation.tsx

---
+++ frontend/hurryup/components/LoadingAnimation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import LottieView from "lottie-react-native";
+
+export default function LoadingAnimation() {
+  return (
+    <View style={styles.container}>
+      <LottieView source={require("../assets/loading.json")} autoPlay loop style={styles.animation} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  animation: { width: 150, height: 150 },
+});
